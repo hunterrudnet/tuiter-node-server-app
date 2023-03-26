6aaa165c1b4812dfbdd693e8de3680c7bc8cc76b
hunterrudnet <hunter.rudnet@gmail.com>
2023-03-26 08:15:30 -0400

    add fields to a9

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,9 +1,26 @@
 import * as tuitsDao from '../../tuits/tuits-dao.js';
 
+const currentUser = {
+  "username": "NASA",
+  "handle": "@nasa",
+  "image": "nasa.png"
+}
+
+const templateTuit = {
+  ...currentUser,
+  "topic": "Space",
+  "time": "2h",
+  "liked": false,
+  "disliked": false,
+  "likes": 0,
+  "dislikes": 0,
+  "replies": 0,
+  "retuits": 0
+}
+
 const createTuit = async (req, res) => {
   const newTuit = req.body;
-  newTuit.likes = 0;
-  newTuit.liked = false;
+  newTuit = {...templateTuit, ...newTuit, _id: (new Date()).getTime()+''};
   const insertedTuit = await tuitsDao.createTuit(newTuit);
   res.json(insertedTuit);
 }
@@ -26,8 +43,6 @@ const deleteTuit = async (req, res) => {
     const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
     res.json(status);
 }
-  
-  
 
 export default (app) => {
  app.post('/api/tuits', createTuit);
@@ -35,3 +50,4 @@ export default (app) => {
  app.put('/api/tuits/:tid', updateTuit);
  app.delete('/api/tuits/:tid', deleteTuit);
 }
+
--- tuits/tuits-schema.js
+++ tuits/tuits-schema.js
@@ -1,7 +1,17 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
+  topic: String,
+  username: String,
+  handle: String,
+  time: String,
+  image: String,
+  title: String,
   tuit: String,
   likes: Number,
+  dislikes: Number,
   liked: Boolean,
+  disliked: Boolean,
+  replies: Number,
+  retuits: Number
 }, {collection: 'tuits'});
 export default schema;
